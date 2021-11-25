d91d1454aa886b8f332066edfc3710d63feac434
soniaelliss <ellissonia5@gmail.com>
2021-11-24 19:35:31 -0800

    added menu and basic functions

---
+++ js/lab.js
@@ -0,0 +1,62 @@
+/*
+ * Authors: Sonia Ellis, Katana Parker, Raymond Wong, Alexis Adolpho, and Michel Nguyen
+ * Created: 11/23/21
+ * License: Public Domain
+ */
+
+
+ //finds the button element
+ buttonEl = document.getElementById("playbutton");
+
+//"Click to play!" button
+ //detects button press
+ buttonEl.addEventListener("click", function(){
+   var fishes = "img/fishes.gif";
+   document.getElementById("bg-pic")
+   .src= fishes;
+
+   var drawnMenu = "img/menu.jpg";
+   document.getElementById("drawn-menu")
+   .src= drawnMenu;
+
+   // to fix: oil image appearing
+   // var oil = "img/oil.png";
+   // document.getElementById("oil")
+   // .src= oil;
+
+ });
+
+//takes user input and returns requested item
+function deadlyItems(str) {
+  word = str;
+  if ((word == "oil") || (word == "Oil")) {
+    return "Oil"
+  }
+  else if ((word == "trash") || (word == "Trash")) {
+    return "Trash"
+  }
+  else if ((word == "plastic") || (word == "Plastic")) {
+    return "Plastic"
+  }
+}
+
+
+//"Get item" button
+//detects a press, gives item description, shows a picture of destroyed reef.
+ $("#input-button").click(function(){
+    var name = document.getElementById("input").value;
+    var item = deadlyItems(name);
+    var newText = "<p>You have chosen " + item + ". This is terrible. The reef has been polluted and living things have been hurt by your actions. </p>";
+    var destroyedReef = "img/deadreef.jpg";
+
+    document.getElementById("output").innerHTML = newText;
+    document.getElementById("deadreef").src = destroyedReef;
+
+ })
+
+ // $("#input-button").classList.toggle("show");
+ //   var name = document.getElementById("input").value;
+ //   var item = deadlyItems(name);
+ //   newText = "<p>You have chosen " + item + ". This is terrible.</p>";
+ //   document.getElementById("output").innerHTML = newText;
+ // })
