31bba0e6fad28e09f272d0a61f12ac515669b4fa
Michel <60534295+michelvn@users.noreply.github.com>
2021-11-30 16:47:12 -0800

    deadreef background changes on click

--- js/lab.js
+++ js/lab.js
@@ -11,74 +11,67 @@
 //"Click to play!" button
  //detects button press
  buttonEl.addEventListener("click", function(){
-   var fishes = "img/fishes.gif";
-   document.getElementById("bg-pic")
-   .src= fishes;
-
-   var drawnMenu = "img/menu.jpg";
-   document.getElementById("drawn-menu")
-   .src= drawnMenu;
-
-   // to fix: oil image appearing
-   // var oil = "img/oil.png";
-   // document.getElementById("oil")
-   // .src= oil;
-
+   document.getElementById("bg-pic").src= "img/fishes.gif";
  });
 
+//button menu
 oilEl = document.getElementById("oil");
 oilEl.addEventListener("click", function(){
   var newText = "<p>You have chosen oil. This is terrible. The reef has been polluted and living things have been hurt by your actions. </p>";
 
   document.getElementById("output").innerHTML = newText;
+  document.getElementById("bg-pic").src= "img/deadreef.jpg";
 })
 
 plasticEl = document.getElementById("plastic");
 plasticEl.addEventListener("click", function(){
-  // var newText = "<p>You have chosen plastic. This is terrible. The reef has been polluted and living things have been hurt by your actions. </p>";
 
   document.getElementById("output").innerHTML = "<p>You have chosen plastic. This is terrible. The reef has been polluted and living things have been hurt by your actions. </p>";
+  document.getElementById("bg-pic").src= "img/deadreef.jpg";
 })
 
 trashEl = document.getElementById("trash");
 trashEl.addEventListener("click", function(){
 
   document.getElementById("output").innerHTML = "<p>You have chosen trash. This is terrible. The reef has been polluted and living things have been hurt by your actions. </p>";
+  document.getElementById("bg-pic").src= "img/deadreef.jpg";
 })
 
 
+//an option we considered for input using a text field and button. Later changed to clicking images.
+//Functional, with this part written by Alexis. Accounts for upper and lowercase inputs as described.
 
-//takes user input and returns requested item
-function deadlyItems(str) {
-  word = str;
-  if ((word == "oil") || (word == "Oil")) {
-    return "Oil"
-  }
-  else if ((word == "trash") || (word == "Trash")) {
-    return "Trash"
-  }
-  else if ((word == "plastic") || (word == "Plastic")) {
-    return "Plastic"
-  }
-}
+    // //takes user input and returns requested item
+    // function deadlyItems(str) {
+    //   word = str;
+    //   if ((word == "oil") || (word == "Oil")) {
+    //     return "Oil"
+    //   }
+    //   else if ((word == "trash") || (word == "Trash")) {
+    //     return "Trash"
+    //   }
+    //   else if ((word == "plastic") || (word == "Plastic")) {
+    //     return "Plastic"
+    //   }
+    // }
 
+            //Would have corresponded to input method described above (line:50)
+            //"Get item" button
+    //detects a press, gives item description, shows a picture of destroyed reef.
+     // $("#input-button").click(function(){
+     //    var name = document.getElementById("input").value;
+     //    var item = deadlyItems(name);
+     //    // var newText = "<p>You have chosen " + item + ". This is terrible. The reef has been polluted and living things have been hurt by your actions. </p>";
+     //    var destroyedReef = "img/deadreef.jpg";
+     //
+     //    document.getElementById("output").innerHTML = newText;
+     //    document.getElementById("deadreef").src = destroyedReef;
+     //
+     // })
 
-//"Get item" button
-//detects a press, gives item description, shows a picture of destroyed reef.
- $("#input-button").click(function(){
-    var name = document.getElementById("input").value;
-    var item = deadlyItems(name);
-    var newText = "<p>You have chosen " + item + ". This is terrible. The reef has been polluted and living things have been hurt by your actions. </p>";
-    var destroyedReef = "img/deadreef.jpg";
-
-    document.getElementById("output").innerHTML = newText;
-    document.getElementById("deadreef").src = destroyedReef;
-
- })
-
- // $("#input-button").classList.toggle("show");
- //   var name = document.getElementById("input").value;
- //   var item = deadlyItems(name);
- //   newText = "<p>You have chosen " + item + ". This is terrible.</p>";
- //   document.getElementById("output").innerHTML = newText;
- // })
+     // $("#input-button").classList.toggle("show");
+     //   var name = document.getElementById("input").value;
+     //   var item = deadlyItems(name);
+     //   newText = "<p>You have chosen " + item + ". This is terrible.</p>";
+     //   document.getElementById("output").innerHTML = newText;
+     // })
